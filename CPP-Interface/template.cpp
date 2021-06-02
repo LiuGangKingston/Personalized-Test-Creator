727e134ebf2736e08c26bdc16c1bc8b9cd8a8975
Gang Liu <gl.cell@outlook.com>
2021-06-02 10:46:11 -0400

    Update template.cpp

--- CPP-Interface/template.cpp
+++ CPP-Interface/template.cpp
@@ -39,12 +39,17 @@ void c_solve_the_question_(int * question_id)
     // End of the predefined varaibles.
 
     // Constant checks with FORTRAN code.
-    verify_c_constants();
+    c_constants_to_be_verified();
+
+    // A constant as an example (more in the *.interface.c file):
+    // strcpy(strff, "BOLTZMANN-CONSTANT\0");
+    // cout << "The constant " << strff << " is " << cget_the_constant_value_of(strff) << endl;
 
 
     switch( * question_id )
     {
-/*	case 60:
+	case 60:
+/*
 
       input_number = 3;
       input_number1 = 4;
@@ -96,5 +101,6 @@ void c_solve_the_question_(int * question_id)
 			break;
     }
 }
+
 
 
